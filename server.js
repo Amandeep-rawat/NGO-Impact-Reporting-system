c6dc61f763dc4a46852c64b1118b3c07cdd33da8
Amandeep-rawat <ar0466467@gmail.com>
2025-04-18 11:07:19 +0530

    new

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const dotenv = require("dotenv")
-const reportRoutes = require("../routes/reportRoutes")
+const reportRoutes = require("./backend/routes/reportRoutes")
 
 // Load environment variables
 dotenv.config()
