a4e66fc2c6a4e5510dd79a49ea1c2248f0f4325a
Amandeep-rawat <ar0466467@gmail.com>
2025-04-18 11:51:49 +0530

    newer iimage

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -19,6 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/favicon.png" type="image/png" />
+
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <main className="min-h-screen bg-background">
